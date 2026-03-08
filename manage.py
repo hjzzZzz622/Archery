e74c326e23da4cb472063934942ea2c96fb781f2
hjzzZzz622 <li.shen.xiamen@gmail.com>
2026-03-08 18:11:25 +0800

    update

--- manage.py
+++ manage.py
@@ -2,6 +2,14 @@
 import os
 import sys
 
+# 使用 pymysql 替代 mysqlclient（requirements-minimal.txt 时无需编译）
+try:
+    import pymysql
+
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "archery.settings")
 
